324f8159f5606629c74507a4ded75d500fb9b041
김 현 KIM HYUN <74993171+stelladream1@users.noreply.github.com>
2023-09-19 03:09:18 +0900

    [Silver II] Title: 부분수열의 합, Time: 332 ms, Memory: 18424 KB -BaekjoonHub

---
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        int[] a = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int result = 0;
+
+        for (int i = 1; i < (1 << n); i++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                if ((i & (1 << j)) != 0) {
+                    sum += a[j];
+                }
+            }
+            if (sum == s) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
