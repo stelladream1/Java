f70ceee7745bbe51d261a7224052e7d3f0e34286
김 현 KIM HYUN <74993171+stelladream1@users.noreply.github.com>
2023-09-20 04:33:26 +0900

    [Silver II] Title: DFS와 BFS, Time: 596 ms, Memory: 39776 KB -BaekjoonHub

---
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -0,0 +1,74 @@
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+    static ArrayList<Integer>[] a;
+    static boolean[] c;
+
+    public static void dfs(int x){
+        if(c[x]==true) return;
+
+        c[x]=true;
+        System.out.print(x+ " ");
+        for(int y: a[x]){
+            if(c[y]== false){
+                dfs(y);
+            }
+        }
+    }
+
+    public static void bfs(int start) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        q.add(start);
+        c[start] = true;
+
+        while(!q.isEmpty()){
+            int x = q.remove();
+            System.out.print(x+ " ");
+            for(int y:a[x]){
+                if(c[y]==false){
+                    c[y]=true;
+                    q.add(y);
+                }
+            }
+        }
+
+    }
+
+
+    public static void main(String args[]) {
+
+       Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();  //정점의 개수
+        int m = sc.nextInt();  // 간선의 개수
+        int start = sc.nextInt(); // 시작할 정점의 번호
+
+        a = (ArrayList<Integer>[]) new ArrayList[n+1];
+
+        for (int i=1; i<=n; i++) {
+            a[i] = new ArrayList<Integer>();
+        }
+        for(int i = 0; i<m; i++){
+            int u = sc.nextInt();
+            int v = sc.nextInt();
+
+            a[u].add(v);
+            a[v].add(u);
+        }
+
+        for(int i =1; i<=n;i++){
+            Collections.sort(a[i]);
+        }
+
+        c = new boolean[n+1];
+        dfs(start);
+        System.out.println();
+        c = new boolean[n+1];
+        bfs(start);
+        
+
+    }
+}
