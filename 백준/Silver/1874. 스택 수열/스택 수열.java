f13d072eca6ffdd161e10fb5ce8d314b54f2801b
김 현 KIM HYUN <74993171+stelladream1@users.noreply.github.com>
2023-09-28 06:47:18 +0900

    [Silver II] Title: 스택 수열, Time: 352 ms, Memory: 26860 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        int start = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
+
+        boolean flag = true;
+        for (int i = 0; i < n; i++)
+        {
+            int num = Integer.parseInt(br.readLine());
+            if (start < num) {
+                for (int j = start + 1; j <= num; j++) {
+                    stack.push(j);
+                    sb.append("+\n");
+                }
+                start = num;
+
+            }
+
+            if (stack.peek() == num) {
+                stack.pop();
+                sb.append("-\n");
+            }
+            else if((start > num) && (stack.peek()!= num) ){
+                flag = false;
+                break;
+            }
+
+        }
+
+        if(flag){
+            System.out.println(sb);
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
