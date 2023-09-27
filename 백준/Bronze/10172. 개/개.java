c77715e4194e6a7b7ad3e6e48a79c9076ae0d796
김 현 KIM HYUN <74993171+stelladream1@users.noreply.github.com>
2023-09-27 10:53:01 +0900

    [Bronze V] Title: 개, Time: 124 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String args[]) throws IOException {
+
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
