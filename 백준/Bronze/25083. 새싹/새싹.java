8ba29d07f89f4972f1e28f9a6e1b76c20b843bac
김 현 KIM HYUN <74993171+stelladream1@users.noreply.github.com>
2023-09-27 10:56:41 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 14052 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String args[]) throws IOException {
+
+
+//        System.out.println("         ,r'\"7");
+//        System.out.println("r`-_   ,'  ,/");
+//        System.out.println(" \\. \". L_r'");
+//        System.out.println("         ,r'\"7");
+//        System.out.println("         ,r'\"7");
+//        System.out.println("         ,r'\"7");
+//
+        System.out.print("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+    }
+}
+
