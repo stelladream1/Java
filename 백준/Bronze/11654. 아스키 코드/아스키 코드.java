e851a04297b1b3952ed0a5f9d57146ab42d441b9
김 현 KIM HYUN <74993171+stelladream1@users.noreply.github.com>
2023-09-27 10:55:21 +0900

    [Bronze V] Title: 아스키 코드, Time: 204 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String args[]) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        char ch = sc.next().charAt(0);
+        
+        System.out.println((int) (ch + 0));
+    }
+}
+
