debd4f60e42609ab528695e31d477300f93af936
김 현 KIM HYUN <74993171+stelladream1@users.noreply.github.com>
2023-09-28 08:02:02 +0900

    [Bronze II] Title: Hashing, Time: 212 ms, Memory: 17744 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
@@ -11,14 +10,17 @@ public class Main {
         long sum = 0;
 
         String st = sc.nextLine();
-        int count = 0;
-        for(char ch: st.toCharArray()){
-  
+        long pow = 1; // 거듭제곱을 저장할 변수
 
-            sum  = (long) (sum + (ch -'0' - 48) * Math.pow(31,count));
-            count = count + 1;
+        for (int i = 0; i < st.length(); i++) {
+            int ch = st.charAt(i) - 'a' + 1; // 문자의 가중치 계산
+
+            // 처음 문자는 그대로 더하고, 이후 문자는 거듭제곱을 곱한 후 더합니다.
+            sum = (sum + (ch * pow) % 1234567891) % 1234567891;
+
+            // 다음 문자를 위해 거듭제곱을 갱신합니다.
+            pow = (pow * 31) % 1234567891;
         }
         System.out.println(sum);
     }
 }
-
