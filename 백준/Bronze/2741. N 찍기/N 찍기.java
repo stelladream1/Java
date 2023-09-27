eeef57b3fc87a41698208011d692add860d6ee2d
김 현 KIM HYUN <74993171+stelladream1@users.noreply.github.com>
2023-09-27 10:27:56 +0900

    [Bronze V] Title: N 찍기, Time: 1152 ms, Memory: 32696 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=1; i<=t; i++){
+            System.out.println(i);
+        }
+        
+    }
+}
+
