e0d0d7da56b34014926ed79ea445eff8ad20be91
김 현 KIM HYUN <74993171+stelladream1@users.noreply.github.com>
2023-09-27 11:03:54 +0900

    [Bronze V] Title: 문자와 문자열, Time: 212 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class Main {
+
+
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int n = sc.nextInt();
+        System.out.println(str.charAt(n-1));
+
+
+    }
+}
+
